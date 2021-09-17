ce4140264e9ba06d80385c69dc774bb3b838f8bf
Joe Hawk <azharrizkizulma@gmail.com>
2021-09-17 13:36:34 +0700

    Create Modul 1 - Materi

---
+++ Praktikum 1/Modul 1 - Materi.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemrograman Dasar
+@Judul: Praktikum 1 Materi
+@Hari/Tanggal: Senin, 20210920
+@NIM: 065001900001
+@author: Azhar Rizky Zulma
+"""
+
+print("haloo")
+print("=========")
+
+angka1 = 10
+angka2 = 30
+
+hasil = angka1 + angka2
+print(hasil)
+
+print("==========")
