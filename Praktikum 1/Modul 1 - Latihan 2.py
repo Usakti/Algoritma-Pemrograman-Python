9d0adc978d3374d0d158c88a602969eafcd1366f
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-09-27 00:07:55 +0700

    Modul 1 - Latihan 2

---
+++ Praktikum 1/Modul 1 - Latihan 2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Variabel dan Pemrograman Dasar
+@Judul: Praktikum 1 Latihan 2
+@Hari/Tanggal: Senin, 20210920
+@NIM: 065001900001
+@author: Azhar Rizky Zulma
+"""
+
+print("Hitung Luas Ruangan")
+
+panjang = float(input("Masukkan Panjang Ruangan: "))
+lebar = float(input("Masukkan Lebar Ruangan: "))
+satuan = input("Masukkan Satuan (Meter/Inci): ")
+
+hasil = panjang * lebar
+
+print("Luas ruangan dengan panjang ", panjang, " dan lebar ", lebar, " adalah ", hasil, " ", satuan)
