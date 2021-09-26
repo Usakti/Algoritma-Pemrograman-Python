5fd9aeeb8bc752035e7046cdabaf1fcb3f4d6f35
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-09-27 00:12:55 +0700

    Modul 2 - Materi

---
+++ Praktikum 2/Modul 2 - Materi.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Operasi Aritmatika
+@Judul: Praktikum 2 Materi
+@Hari/Tanggal: Senin, 20210927
+@NIM: 065001900001
+@author: Azhar Rizky Zulma
+"""
+
+# import math
+
+angka1 = 10
+angka2 = 30
+
+hasil = angka1 + angka2
+print(hasil)
+
+"""
+print("===============")
+
+a = math.pi/2
+
+print(math.sin(a))
+
+print(math.sin(math.radians(30)))
+"""
