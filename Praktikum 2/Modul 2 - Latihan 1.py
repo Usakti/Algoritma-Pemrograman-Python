91f37c4262f88aff237bdd4bd20f9eeff95b6d69
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-09-27 16:30:01 +0700

    Create Modul 2 Latihan 1

---
+++ Praktikum 2/Modul 2 - Latihan 1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+@Materi: Operasi Aritmatika
+@Judul: Praktikum 2 Latihan 1
+@Hari/Tanggal: Senin, 20210920
+@NIM: 065001900001
+@author: Azhar Rizky Zulma
+"""
+
+import math
+
+a = float(input("Masukkan nilai a = "))
+b = float(input("Masukkan nilai b = "))
+
+print("Jumlah a ditambah b adalah ", a+b)
+print("Selisih antara a dan b adalah ", abs(b-a))
+print("Jumlah a dikali b adalah ", a*b)
+print("Jumlah sisa pembagian dari hasil a dibagi b adalah ", a%b)
+print("Jumlah a dibagi b adalah ", a/b)
+print("Hasil dari log(a) adalah ", math.log10(a))
+print("Hasil a pangkat b adalah ", a**b)
