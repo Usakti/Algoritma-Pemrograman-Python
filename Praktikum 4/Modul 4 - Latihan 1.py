731f1ac7bbc7a72ef296a3692338d6b3b97b4876
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-10-11 15:34:25 +0700

    Update Latihan 1

--- Praktikum 4/Modul 4 - Latihan 1.py
+++ Praktikum 4/Modul 4 - Latihan 1.py
@@ -7,6 +7,15 @@
 @author: Azhar Rizky Zulma
 """
 
+# Versi 1
+a = int(input("masukkan jumlah maksimal: "))
+for i in range(a, 0, -1):
+    print(i)
+    for j in range(0, i):
+        print(i, end='')
+    print()
+
+# Versi 2
 a = int(input("masukkan jumlah maksimal: "))
 for i in range(-1, a):
     for j in range(-1, a - 1):
