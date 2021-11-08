22bc75438ca55ed72420dda08b9b81f3ee3129d6
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-11-08 14:02:11 +0700

    Update Modul 6 Latihan 2

--- Praktikum 7/Modul 6 - Latihan 2.py
+++ Praktikum 7/Modul 6 - Latihan 2.py
@@ -7,19 +7,21 @@
 @author: Azhar Rizky Zulma
 """
 
+
 print ("Program ini akan menentukan jumlah hari dalam bulan tertentu")
 
 F = False
 
 year = ''
 
+#Fungsi Tidak Berparameter
 def Kabisat(year):
-    year = int(input("masukkan tahun (e.g., 2019): "))
     if (year % 4 == 0) and (not (year % 100 == 0) or (year % 400 == 0)):
         print("29 hari dalam sebulan")
     else:
         print("28 hari dalam sebulan")
-    
+   
+#Fungi Berparameter
 def NonKabisat(bulan):
     if bulan in [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12]:
         if bulan in [4, 6, 9, 11]:
@@ -27,7 +29,8 @@ def NonKabisat(bulan):
         elif bulan in [1, 3, 5, 7, 8, 10, 12]:
             print("31 hari dalam sebulan")
         elif bulan == 2:
-            Kabisat(year)
+            year = int(input("masukkan tahun (e.g., 2021): "))
+            Kabisat(year)  #Melempar Nilai Kedalam Fungsi Berparameter
     else:
         print("Error")
             
@@ -37,6 +40,6 @@ while (not F):
     if bulan == 0:
         F = True
     else:
-        NonKabisat (bulan)
+        NonKabisat(bulan) #Melempar Nilai Kedalam Fungsi Berparameter
     
 print ("Terima kasih sudah menggunakan program saya, sampai berjumpa lagi.")
