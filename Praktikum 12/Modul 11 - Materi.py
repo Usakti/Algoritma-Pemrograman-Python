ede79cec3b775ce8b8b6853e8d94dee1ccb1081a
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-12-06 13:40:04 +0700

    Update Modul 11 - Materi

--- Praktikum 12/Modul 11 - Materi.py
+++ Praktikum 12/Modul 11 - Materi.py
@@ -29,3 +29,25 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print ("Total Employee %d" % Employee.empCount)
+
+
+#Versi Mamalia
+
+class Mamalia:
+    mamalCount = 0
+    
+    def __init__(self, nama_mamalia, kaki_mamalia):
+        self.nama_mamalia = nama_mamalia
+        self.kaki_mamalia = kaki_mamalia
+        Mamalia.mamalCount += 1
+        
+    def printMamal(self):
+        print("Nama Mamalia ini Adalah: " + self.nama_mamalia + "\nKakinya Berjumlah " + str(self.kaki_mamalia))
+                
+mamal1 = Mamalia("Kucing", 4)
+mamal1.printMamal()
+
+mamal2 = Mamalia("Hamster", 4)
+mamal2.printMamal()
+
+print("Jumlah Mamalia yang dimasukkan ke dalam objek adalah: ", Mamalia.mamalCount)
