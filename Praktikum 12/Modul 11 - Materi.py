d2106da937ec6529ebb6eea62fff499966063d3b
Azhar Rizki Zulma <71958079+AzharRizky@users.noreply.github.com>
2021-12-06 12:08:50 +0700

    Create Modul 11 - Materi

---
+++ Praktikum 12/Modul 11 - Materi.py
@@ -0,0 +1,31 @@
+"""
+@Materi: Object Oriented Programming
+@Judul: Praktikum 11 Materi
+@Hari/Tanggal: Senin, 20210920
+@NIM: 065001900001
+@author: Azhar Rizky Zulma
+"""
+
+class Employee:
+   'Common base class for all employees'
+   empCount = 0
+
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print ("Total Employee %d" % Employee.empCount)
+
+   def displayEmployee(self):
+      print ("Name: ", self.name,  ", Salary: ", self.salary)
+
+
+#This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+#This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print ("Total Employee %d" % Employee.empCount)
